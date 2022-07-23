fdaf5c042fde554e58f8f9d652659a6dc5552fa5
Ivanov Alexander <ivanovaa1992@gmail.com>
2022-07-23 21:00:01 +0300

    init commit

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <>
+    <Head>
+        <title>MyTop</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      </Head>
+    <Component {...pageProps}></Component>
+  </>
+}
+
+export default MyApp
