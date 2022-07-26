8caced5fad750094f48d2f3f712a748ac136034f
Ivanov Alexander <ivanovaa1992@gmail.com>
2022-07-26 23:07:00 +0300

    Rating

--- pages/components/index.ts
+++ pages/components/index.ts
@@ -2,3 +2,4 @@ export { Htag } from './Htag/Htag';
 export { Button } from './Button/Button';
 export { Paragraph } from './Paragraph/Paragraph';
 export { Tag } from './Tag/Tag';
+export { Rating } from './Rating/Rating';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
-import React from "react";
-import { Button, Htag, Paragraph, Tag } from "./components/index";
+import React, {useEffect, useState} from "react";
+import { Button, Htag, Paragraph, Rating, Tag } from "./components/index";
 
 
 export default function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
+
+  useEffect(() => {
+    console.log(counter)
+    return function cleanup() {
+      console.log('sdfsd')
+    }
+   })
+
   return (
     <div>
-      <Htag tag='h1'>Текст</Htag>
-      <Button appearence='primary' arrow='right'>Кнопка</Button>
+      <Htag tag='h1'>{counter}</Htag>
+      <Button appearence='primary' arrow='right' onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
       <Button appearence='ghost' arrow='right' className="hi">Кнопка-1</Button>
       <Paragraph className="hi">Студенты освоят не только hard skills, необходимые для работы веб-дизайнером, но и soft skills — навыки, которые позволят эффективно взаимодействовать в команде с менеджерами, разработчиками и маркетологами. Выпускники факультета могут успешно конкурировать с веб-дизайнерами уровня middle.</Paragraph>
       <Tag color="red">средний</Tag>
+      <Rating rating={3}></Rating>
     </div>
   );
 }
