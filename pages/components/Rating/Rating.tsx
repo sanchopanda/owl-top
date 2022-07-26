82c8dab3c5b3158d433c0a8f7f3adf211d262f2a
Ivanov Alexander <ivanovaa1992@gmail.com>
2022-07-26 23:38:15 +0300

    update rating

--- pages/components/Rating/Rating.tsx
+++ pages/components/Rating/Rating.tsx
@@ -1,7 +1,7 @@
 import { RatingProps } from "./Rating.props";
 import styles from "./Rating.module.css";
 import cn from "classnames";
-import { useState, useEffect } from "react";
+import { useState, useEffect, KeyboardEvent } from "react";
 import StarIcon from "./star.svg";
 
 export const Rating = ({
@@ -11,30 +11,56 @@ export const Rating = ({
   className,
   ...props
 }: RatingProps): JSX.Element => {
-
   const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
     new Array(5).fill(<></>)
   );
 
   useEffect(() => {
     constructRating(rating);
-  }, [rating])
+  }, [rating]);
 
   const constructRating = (currentRating: number) => {
     const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
-      return <StarIcon
+      return <span
         className={cn(styles.star, {
           [styles.filled]: i < currentRating,
+          [styles.editable]: isEditable,
         })}
-      />;
-    });   
+        onMouseEnter={() => changeDisplay(i + 1)}
+        onMouseLeave={() => changeDisplay(rating)}
+        onClick={() => onclick(i + 1)}
+      >
+        <StarIcon
+          tabIndex={isEditable ? 0 : -1}
+          onKeyDown={(e: KeyboardEvent<SVGElement>) =>
+            isEditable && handleSpace(i + 1, e)
+          }
+        />
+      </span>;
+    });
     setRatingArray(updatedArray);
   };
 
+  const changeDisplay = (i: number) => {
+    if (!isEditable) return;
+    constructRating(i);
+  };
+
+  const onclick = (i: number) => {
+    if (!isEditable || !setRating) return;
+    setRating(i);
+  };
+
+  const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
+    if (e.code != "Space" || !setRating) return;
+    setRating(i);
+  };
+
   return (
-      <div
-        {...props}>
-        {ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
-      </div>
+    <div {...props}>
+      {ratingArray.map((r, i) => (
+        <span key={i}>{r}</span>
+      ))}
+    </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,16 @@ import { Button, Htag, Paragraph, Rating, Tag } from "./components/index";
 
 
 export default function Home(): JSX.Element {
-  const [counter, setCounter] = useState<number>(0);
-
-  useEffect(() => {
-    console.log(counter)
-    return function cleanup() {
-      console.log('sdfsd')
-    }
-   })
+  const [rating, setRating] = useState<number>(4);
 
   return (
     <div>
-      <Htag tag='h1'>{counter}</Htag>
-      <Button appearence='primary' arrow='right' onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
+      <Htag tag='h1'>Заголовок</Htag>
+      <Button appearence='primary' arrow='right' >Кнопка</Button>
       <Button appearence='ghost' arrow='right' className="hi">Кнопка-1</Button>
       <Paragraph className="hi">Студенты освоят не только hard skills, необходимые для работы веб-дизайнером, но и soft skills — навыки, которые позволят эффективно взаимодействовать в команде с менеджерами, разработчиками и маркетологами. Выпускники факультета могут успешно конкурировать с веб-дизайнерами уровня middle.</Paragraph>
       <Tag color="red">средний</Tag>
-      <Rating rating={3}></Rating>
+      <Rating isEditable={true} rating={rating} setRating={setRating}></Rating>
     </div>
   );
 }
