2b393fa5ad0525379f0defd8a82beb6e8627885a
Ivanov Alexander <ivanovaa1992@gmail.com>
2022-07-28 22:08:02 +0300

    Layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,31 +1,30 @@
 import { LayoutProps } from "./Layout.props";
 import styles from "./Layout.module.css";
-import cn from 'classnames';
+import cn from "classnames";
 import { Header } from "./Header/Header";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Footer } from "./Footer/Footer";
 import { Component, FunctionComponent } from "react";
 
-export const Layout = ({ children,  ...props }: LayoutProps): JSX.Element => { 
-  return <>
-    <Header/>
-    <div>
-        <Sidebar/>
-        <div>
-            {children}
-        </div>
+export const Layout = ({ children, ...props }: LayoutProps): JSX.Element => {
+  return (
+    <div className={styles.wrapper}>
+      <Header className={styles.header}/>
+      <Sidebar className={styles.sidebar}/>
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer}/>
     </div>
-    <Footer/>
-  </>
+  );
 };
 
-
-export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
-    return function withLayoutComponent(props: T) {
-        return (
-            <Layout>
-                <Component {...props}/>
-            </Layout>
-        )
-    }
-} 
+export const withLayout = <T extends Record<string, unknown>>(
+  Component: FunctionComponent<T>
+) => {
+  return function withLayoutComponent(props: T) {
+    return (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    );
+  };
+};
